9b168156fc3c44395f401d2ba118dbc66b80a81b
oliveriandrea <oliveriandrea@gmail.com>
2016-10-26 11:50:58 +0200

    Delete old davical database

--- restore_db.sh
+++ restore_db.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+su - postgres -c 'psql -c "drop database davical;"'
 su - postgres -c 'createdb --owner davical_dba --encoding UTF8 --template template0 davical'
 su postgres -c 'pg_restore -Ft -d davical /config/davical_backup.tar'
 /usr/share/davical/dba/update-davical-database
